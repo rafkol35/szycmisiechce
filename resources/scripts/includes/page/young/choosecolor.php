be30f6b89e7575bac0daf64029a59edcd1d18446
rafkol <rafalkolasa@o2.pl>
2015-06-03 10:37:34 +0200

    dalsze prace

--- resources/scripts/includes/page/young/choosecolor.php
+++ resources/scripts/includes/page/young/choosecolor.php
@@ -26,6 +26,13 @@ var colBs = [0,128,255,128,0];//new Array();
 
 var color;
 
+var ready = false;
+var masksToLoad = 5;
+var loadedMasks = 0;
+
+var lastMousePosX = 0;
+var lastMousePosY = 0;
+
 function pick(event) {
 	var x = event.layerX;
 	var y = event.layerY;
@@ -37,38 +44,21 @@ function pick(event) {
 	color.textContent = " " + x + " " + y + " " + rgba;	  
 }
 
-var masksToLoad = 5;
-var loadedMasks = 0;
-
-function imgMainReady() {
-
-	//colRs.push();
-	//cols.push();
-	//colRs.push();
-	
-	  ctxmain.drawImage(imgmain, 0, 0);
-	  imgmain.style.display = 'none';
-	  canvasmain.style.display = 'none';
-	  	
-	  for( var i = 0 ; i < masksToLoad ; ++i ){
-	  	imgmasks.push( new Image() );	  
-	  	//imgmasks[0].src = '<?php echo base_url('resources/images/content/young/choosetype/type04_pas1.png'); ?>';
-	  	imgmasks[i].src = '<?php echo base_url('resources/images/content/young/choosetype'); ?>'+'/type04_pas'+(i+1)+'.png';
-	  	canvasmasks.push( document.getElementById('canvasmask'+(i+1)) );
-	  	ctxmasks.push( canvasmasks[i].getContext('2d') );
-	  	imgmasks[i].onload = imgMaskLoadCounter;
-	  }
-};
-
 function imgMaskLoadCounter(){
 	loadedMasks++;
-	if( loadedMasks == masksToLoad ){
-		imgMaskReady();
+	if( loadedMasks == (masksToLoad+1) ){
+		redrawAll();
 	}
 }
 
-function imgMaskReady() {
-	  var imageMainData = ctxmain.getImageData(0,0,canvasmain.width, canvasmain.height);
+function redrawAll(){
+	//rysuje bazowy obrazek
+	ctxmain.clearRect(0, 0, canvasmain.width, canvasmain.height);
+	
+	ctxmain.drawImage(imgmain, 0, 0);
+	
+	// rysuje pasy
+	var imageMainData = ctxmain.getImageData(0,0,canvasmain.width, canvasmain.height);
 	  var dataMain = imageMainData.data;
 
 	  for( var i = 0 ; i < masksToLoad ; ++i ){
@@ -77,8 +67,6 @@ function imgMaskReady() {
 		  var ctxmask = ctxmasks[i]; 
 		  
 		  ctxmask.drawImage(imgmask, 0, 0);
-		  imgmask.style.display = 'none';
-		  canvasmask.style.display = 'none';
 		  
 		  var imageMaskData = ctxmask.getImageData(0,0,canvasmask.width, canvasmask.height);
 		  var dataMask = imageMaskData.data;
@@ -98,8 +86,39 @@ function imgMaskReady() {
 		  ctxmain.putImageData(imageMainData, 0, 0);
 		  canvasmain.style.display = 'inherit';
 	  }
+}
+
+function onMouseMove(event){
+	var msg = "";
+	//msg += event.clientX + ", " + event.clientY;
+	//msg += " " + event.pageX + ", " + event.pageY;
+
+	var parentOffset = $(this).parent().offset(); 
+	lastMousePosX = Math.floor( event.pageX - parentOffset.left );
+	lastMousePosY = Math.floor( event.pageY - parentOffset.top );
+	msg += "    " + lastMousePosX + ", " + lastMousePosY;
 	  
-};
+	$( "#color" ).text( msg );
+}
+
+function myRand(min,max){
+	return Math.floor((Math.random() * max) + min);
+}
+
+function onMouseDown(){
+	for( var i = 0 ; i < masksToLoad ; ++i ){
+
+		var pixel = ctxmasks[i].getImageData(lastMousePosX,lastMousePosY,1,1);
+		var data = pixel.data;
+		
+		if( data[3] > 2 ){
+			colRs[i] = myRand(0, 255);
+			colGs[i] = myRand(0, 255);
+			colBs[i] = myRand(0, 255);
+			redrawAll();
+		} 
+	}
+}
 
 $(document).ready(function() {
     menuready();
@@ -107,24 +126,26 @@ $(document).ready(function() {
     imgmain.src = '<?php echo base_url('resources/images/content/young/choosetype/type04_org.png'); ?>';
     canvasmain = document.getElementById('canvasmain');
 	ctxmain = canvasmain.getContext('2d');
-	imgmain.onload = imgMainReady;
+	imgmain.onload = imgMaskLoadCounter;
 
+	imgmain.style.display = 'none';
+	canvasmain.style.display = 'none';
+
+	 for( var i = 0 ; i < masksToLoad ; ++i ){
+		  	imgmasks.push( new Image() );	  
+		  	imgmasks[i].src = '<?php echo base_url('resources/images/content/young/choosetype'); ?>'+'/type04_pas'+(i+1)+'.png';
+		  	canvasmasks.push( document.getElementById('canvasmask'+(i+1)) );
+		  	ctxmasks.push( canvasmasks[i].getContext('2d') );
+		  	imgmasks[i].onload = imgMaskLoadCounter;
+
+		  	imgmasks[i].style.display = 'none';
+			canvasmasks[i].style.display = 'none';
+		  }
+	
 	color = document.getElementById('color');
 	//canvasmain.addEventListener('mousemove', pick);
 	
-	$( "#canvasmain" ).mousemove(function( event ) {
-  		var msg = "";
-  		//msg += event.clientX + ", " + event.clientY;
-  		//msg += " " + event.pageX + ", " + event.pageY;
-
-  		var parentOffset = $(this).parent().offset(); 
-  		var relX = event.pageX - parentOffset.left;
-  	   	var relY = event.pageY - parentOffset.top;
-  	  	msg += "    " + relX + ", " + relY;
-  	  
-  		$( "#color" ).text( msg );
-	});
-	
-    //draw();
+	$( "#canvasmain" ).on('mousemove',onMouseMove);
+	$( "#canvasmain" ).on('mousedown',onMouseDown);
 });
 </script>
